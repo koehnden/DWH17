7112b192ae73d01f7846ab6f5e0e8eefd62d91b6
Dennis Köhn <dennis.koehn@webtrekk.com>
2017-11-05 17:21:35 +0100

    delete unecessary code

--- ScalableIntersection/src/main/java/NaiveJoin.java
+++ ScalableIntersection/src/main/java/NaiveJoin.java
@@ -1,21 +1,7 @@
 import java.util.*;
 
 public class NaiveJoin {
-
-    public Set<String> joinOpt(Map<String, Character> firstFile, Map<String, Character> secondFile) {
-        Set<String> resultSet = new HashSet<String>();
-        System.out.println("Start Joining Files...");
-        for (String firstKey : firstFile.keySet()) {
-            for (String secondKey : secondFile.keySet()) {
-                if (firstKey.equals(secondKey)) {
-                    resultSet.add(firstFile.get(firstKey).toString() + firstKey);
-                    resultSet.add(secondFile.get(secondKey).toString() + secondKey);
-                }
-            }
-        }
-        return resultSet;
-    }
-
+    
     public Set<String> join(Map<String, Character> firstFile, Map<String, Character> secondFile) {
         Set<String> resultSet = new HashSet<String>();
         System.out.println("Start Joining Files...");
