de126e2a62471ed6e002a7ba253e563f58bab12e
Malte <MaltteKruse1@gmx.de>
2018-02-07 18:12:52 +0100

    added contest queries

--- NaiveBayes/test.sql
+++ NaiveBayes/test.sql
@@ -1,3 +1,48 @@
+create or replace FUNCTION wettbewerb(
+    m_sex IN cruise_train.sex%TYPE,
+    m_class IN cruise_train.pclass%TYPE,
+    m_age in cruise_train.age%TYPE,
+    m_fare in cruise_train.fare%TYPE
+)
+RETURN NUMBER 
+AS 
+    prediction NUMBER := 0;
+BEGIN 
+
+
+	SELECT calc.label into prediction
+	FROM (SELECT 
+			label, 
+			sex,
+			pclass,
+			(cnt.num / sum(count(*)) over (PARTITION BY label))
+            * (cntf.num / sum(count(*)) over (PARTITION BY label))
+			* (sum(count(*)) over (PARTITION BY pclass, label) / sum(count(*)) over (PARTITION BY label))
+			* (sum(count(*)) over (PARTITION BY sex, label) / sum(count(*)) over (PARTITION BY label)) 
+			* (sum(count(*)) over  (PARTITION by (label)) /  sum(count(*)) over ()) as per
+          FROM CRUISE_TRAIN, (SELECT t.label as lab, count(*) as num
+                              FROM CRUISE_TRAIN t
+                              WHERE t.age <= (m_age +4) or ((m_age is null or m_age < 1) and t.age is not null)
+                              GROUP BY t.label) cnt, (SELECT tf.label as lab, count(*) as num
+                                                     FROM CRUISE_TRAIN tf
+                                                     WHERE tf.fare <= m_fare * 1.05
+                                                     GROUP BY tf.label) cntf
+        WHERE label = cnt.lab and label = cntf.lab
+		GROUP BY label, sex, pclass, cnt.num, cntf.num
+		ORDER BY per desc) calc
+	WHERE calc.sex = m_sex and calc.pclass = m_class and rownum = 1; 
+
+    DBMS_OUTPUT.PUT_LINE('prediction = ' || prediction);    
+    return prediction;
+END;
+
+----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
+
+
 --SELECT label, count(*) / sum(count(*)) over () as percent
 --FROM CRUISE_TRAIN
 --GROUP BY label;
@@ -36,3 +81,31 @@ SELECT
 FROM CRUISE_TRAIN
 GROUP BY label, sex, pclass
 ORDER BY per desc;
+
+
+----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
+
+
+SELECT 
+    label, 
+    sex,
+    pclass,
+    embarked,
+--    age,
+--    sum(count(*)) over (PARTITION BY label, age) as num_age,
+    sum(count(*)) over (PARTITION BY label, embarked) as num_embarked,
+    sum(count(*)) over (PARTITION BY sex,label) as n_sex,
+    sum(count(*)) over (PARTITION BY pclass, label) as num_sex_class,
+    sum(count(*)) over (PARTITION BY label) max_num_sex
+--    ,
+--    (sum(count(*)) over (PARTITION BY pclass, label) / sum(count(*)) over (PARTITION BY label))
+--   * (sum(count(*)) over (PARTITION BY sex, label) / sum(count(*)) over (PARTITION BY label)) 
+--    * (sum(count(*)) over  (PARTITION by (label)) /  sum(count(*)) over ()) as per
+FROM CRUISE_TRAIN
+--WHERE age IS NOT NULL
+GROUP BY label, sex, pclass, embarked, age;
+--ORDER BY per desc;
---
+++ NaiveBayes/wettbewerb.sql
@@ -0,0 +1,73 @@
+create or replace FUNCTION wettbewerb(
+    m_sex IN cruise_train.sex%TYPE,
+    m_class IN cruise_train.pclass%TYPE,
+    m_age in cruise_train.age%TYPE,
+    m_fare in cruise_train.fare%TYPE
+)
+RETURN NUMBER 
+AS 
+    prediction NUMBER := 0;
+BEGIN 
+
+
+	SELECT calc.label into prediction
+	FROM (SELECT 
+			label, 
+			sex,
+			pclass,
+			(cnt.num / sum(count(*)) over (PARTITION BY label))
+            * (cntf.num / sum(count(*)) over (PARTITION BY label))
+			* (sum(count(*)) over (PARTITION BY pclass, label) / sum(count(*)) over (PARTITION BY label))
+			* (sum(count(*)) over (PARTITION BY sex, label) / sum(count(*)) over (PARTITION BY label)) 
+			* (sum(count(*)) over  (PARTITION by (label)) /  sum(count(*)) over ()) as per
+          FROM CRUISE_TRAIN, (SELECT t.label as lab, count(*) as num
+                              FROM CRUISE_TRAIN t
+                              WHERE t.age <= (m_age + 4) or (m_age is null and t.age is null)
+                              GROUP BY t.label) cnt, (SELECT tf.label as lab, count(*) as num
+                                                     FROM CRUISE_TRAIN tf
+                                                     WHERE tf.fare <= m_fare * 1.05
+                                                     GROUP BY tf.label) cntf
+        WHERE label = cnt.lab and label = cntf.lab
+		GROUP BY label, sex, pclass, cnt.num, cntf.num
+		ORDER BY per desc) calc
+	WHERE calc.sex = m_sex and calc.pclass = m_class and rownum = 1; 
+
+    DBMS_OUTPUT.PUT_LINE('prediction = ' || prediction);    
+    return prediction;
+END;
+
+
+create or replace FUNCTION eval_wettbewerb
+RETURN NUMBER 
+AS 
+  accuracy NUMBER := 0;
+  TP NUMBER := 0;
+  FP NUMBER := 0;
+  TN NUMBER := 0;
+  FN NUMBER := 0;
+  res NUMBER := 0;
+BEGIN
+	FOR r in (SELECT sex, label, pclass, age, fare
+			  FROM CRUISE_TRAIN)
+	LOOP
+		SELECT wettbewerb(r.sex, r.pclass, r.age, r.fare) into res
+		FROM DUAL;
+		IF res = 1 AND r.label = 1 THEN
+			TP := TP + 1;
+		ELSIF res = 1 AND r.label = 0 THEN
+			FP := FP + 1;
+		ELSIF res = 0 AND r.label = 0 THEN
+			TN := TN + 1;
+		ELSIF res = 0 AND r.label = 1 THEN
+			FN := FN + 1;
+		END IF;
+	END LOOP;
+	accuracy := (TP + TN)/(TP + TN + FP + FN);
+    DBMS_OUTPUT.PUT_LINE('accuracy = ' || accuracy);    
+    RETURN accuracy;
+END;
+
+
+CREATE OR REPLACE VIEW wettbewerb_calc AS
+SELECT wettbewerb(sex, pclass, age, fare) as label
+FROM CRUISE_TEST;
